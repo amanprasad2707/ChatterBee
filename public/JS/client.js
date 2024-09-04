e674d115ff9664228fa1f4ee74871dd9ee6f8d1b
Aman Prasad <121761736+amanprasad2707@users.noreply.github.com>
2024-09-04 15:26:25 +0530

    change bot name

--- public/JS/client.js
+++ public/JS/client.js
@@ -92,7 +92,7 @@ function appendAiResponse(msg, type) {
   // Format the message by replacing newline characters with HTML line breaks
   const formattedMsg = msg.replace(/\n/g, '<br>'); // Replace \n with <br>
   const markup = `
-    <h4>${type === 'outgoing' ? 'You' : 'Codehelp'}:</h4>
+    <h4>${type === 'outgoing' ? 'You' : 'careerhelp'}:</h4>
     <p>${formattedMsg}</p>
   `;
   mainDiv.innerHTML = markup;
